a7f67cefa9149520b3a3a2b3530d87603869cd52
NielsVandenBroeck <73421746+NielsVandenBroeck@users.noreply.github.com>
2024-07-02 17:26:41 +0200

    Update create_event.py

--- create_event.py
+++ create_event.py
@@ -4,7 +4,7 @@ def createEvent(startDate, endDate, name, colleagues):
    service = get_calendar_service()
    #mpeei3q77ufnpq04rvl1vrgsm8@group.calendar.google.com   test
    #070k5lmlvnb1ohd7326sab9d8g@group.calendar.google.com   werk Niels
-   idDict = {"Roos": "mpeei3q77ufnpq04rvl1vrgsm8@group.calendar.google.com", "Niels": "070k5lmlvnb1ohd7326sab9d8g@group.calendar.google.com"}
+   idDict = {"Niels": "070k5lmlvnb1ohd7326sab9d8g@group.calendar.google.com"}
    if name in idDict:
        id = idDict[name]
    else:
